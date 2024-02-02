354e3bc121dcdaf263117c6a5a7b72746e42b4ff
pmp-p <mail.peny@free.fr>
2024-02-02 19:13:02 +0100

    add wasi pview test checksum

--- cmake-builds.sh
+++ cmake-builds.sh
@@ -22,22 +22,22 @@ then
     echo "building  ${SRCDIR} for host $(arch)"
 
     CC=clang CXX=clang++ ${SDKROOT}/devices/x86_64/usr/bin/cmake -S ${SRCDIR} \
-     -DCMAKE_BUILD_TYPE=Debug -DHAVE_AUDIO=NO -DHAVE_THREADS=NO \
+     -DCMAKE_BUILD_TYPE=Debug -DHAVE_AUDIO=YES -DHAVE_THREADS=NO \
      -DHAVE_EGL=NO -DHAVE_GL=YES -DHAVE_GLX=YES -DHAVE_X11=YES -DHAVE_GLES1=NO -DHAVE_GLES2=YES \
      -DHAVE_OPENSSL=NO \
  -DHAVE_NET=NO -DWANT_NATIVE_NET=NO -DDO_PSTATS=NO \
-     -DHAVE_ZLIB=YES -DHAVE_PNG=NO  \
-     -DHAVE_TIFF=NO -DHAVE_JPEG=NO \
+     -DHAVE_ZLIB=YES -DHAVE_PNG=YES  \
+     -DHAVE_TIFF=NO -DHAVE_JPEG=YES \
      -DHAVE_OPUS=NO \
      -DHAVE_HARFBUZZ=YES -DHAVE_FREETYPE=YES \
- -DHAVE_IOS_TYPEDEFS=1 -DHAVE_TINYDISPLAY=1 -DBUILD_SHARED_LIBS=NO \
+ -DHAVE_IOS_TYPEDEFS=1 -DHAVE_TINYDISPLAY=1 -DBUILD_SHARED_LIBS=YES \
     \
      -DCMAKE_INSTALL_PREFIX=${SDKROOT}/devices/$(arch)/usr \
      -DHAVE_PYTHON=1 -DPython_ROOT_DIR=${SDKROOT}/devices/$(arch)/usr \
-    -DWANT_PYTHON_VERSION=3.12
+     -DWANT_PYTHON_VERSION=${PYBUILD}
 
 
-
+# https://cmake.org/cmake/help/v3.13/module/FindPython.html
 #     -DPython_VERSION=3.12 -DPython_VERSION_MAJOR=3 -DPython_VERSION_MINOR=12 -DPython_VERSION_PATCH=1 \
 #        -DPython3_EXECUTABLE:FILEPATH=${SDKROOT}/devices/$(arch)/usr/bin/python${PYBUILD} \
 #        -DPython3_INCLUDE_DIR=${SDKROOT}/devices/$(arch)/usr/include/python${PYBUILD} \
@@ -47,7 +47,6 @@ then
 #        -DPython3_Development.Module_FOUND=TRUE \
 #        -DPython3_Development.Embed_FOUND=TRUE \
 #    \
-
 # -DHAVE_SDL2=1
 
 
@@ -127,19 +126,25 @@ END
      -DHAVE_THREADS=NO \
      -DHAVE_EGL=NO -DHAVE_GL=NO -DHAVE_GLX=NO -DHAVE_X11=NO -DHAVE_GLES1=NO -DHAVE_GLES2=NO \
      -DHAVE_OPENSSL=NO \
-     -DHAVE_AUDIO=NO -DHAVE_OPUS=NO \
+ -DHAVE_AUDIO=1 -DHAVE_PYTHON=0\
+ -DHAVE_OPUS=NO \
      -DHAVE_HARFBUZZ=NO -DHAVE_FREETYPE=NO \
      -DPHAVE_IOSTREAM=1 -DHAVE_TINYDISPLAY=1 -DHAVE_TIFF=NO  \
     \
     -DCMAKE_SYSTEM_NAME=WASI \
+    -DWASISDK=${SDKROOT}/wasisdk \
+    -DWASI_SDK_PREFIX=${WASI_SDK_PREFIX} \
     \
      -DHAVE_NET=NO -DWANT_NATIVE_NET=NO -DDO_PSTATS=NO \
      -DHAVE_PYTHON=NO -DZLIB_ROOT=${WASI_SYSROOT} \
     \
-     -DWASISDK=${SDKROOT}/wasisdk \
-     -DWASI_SDK_PREFIX=${WASI_SDK_PREFIX} \
+     -DHOST_PATH_PZIP=/opt/python-wasm-sdk/build/panda3d-host/bin/pzip \
      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DHOST_PATH_PZIP=/opt/python-wasm-sdk/build/panda3d-host/bin/pzip
+
+
+#     -DHAVE_PYTHON=1 -DPython_ROOT_DIR=${SDKROOT}/devices/wasisdk/usr \
+#     -DWANT_PYTHON_VERSION=${PYBUILD}
+
 
 #      -DHAVE_ZLIB=0 -DHAVE_PNG=NO -DHAVE_JPEG=NO \
 # -DCMAKE_SYSTEM_NAME=WASI
@@ -173,4 +178,19 @@ fi
 
 make
 
+
+if $CI
+then
+    ${SDKROOT}/wasisdk/bin/wasi-run ./bin/pview.wasi -c -L -s $(pwd)/out.bmp
+    if md5sum out.bmp | grep -q "a0345fc5e8b77b9715df69e271e22c64"
+    then
+        exit 0
+    else
+        echo "error screen output checksum does not match"
+        exit 5
+    fi
+else
+    echo not running test
+    exit 0
+fi
 
