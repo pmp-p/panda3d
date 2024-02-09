6318cf2ee8dcc8db5aa19bd81dc9c2cf26b3674c
pmp-p <mail.peny@free.fr>
2024-02-09 21:02:39 +0100

    native/wasi build

--- cmake-builds.sh
+++ cmake-builds.sh
@@ -4,37 +4,53 @@
 export PYBUILD=${PYBUILD:-3.12}
 export SDKROOT=${SDKROOT:-/opt/python-wasm-sdk}
 
-export SRCDIR=$(realpath $(dirname $0))
+export BUILD_DIR=$(pwd)
+export SRC_DIR=$(realpath $(dirname $0))
 
-echo SRCDIR=$SRCDIR
+echo SRC_DIR=$SRC_DIR
 
 if ${CI:-false}
 then
-    pushd ${SRCDIR}
+    pushd ${SRC_DIR}
     wget -O- https://patch-diff.githubusercontent.com/raw/pmp-p/panda3d/pull/10.diff | patch -p1
+    wget -O- https://patch-diff.githubusercontent.com/raw/panda3d/panda3d/pull/1611.diff | patch -p1
+    wget -O- https://patch-diff.githubusercontent.com/raw/panda3d/panda3d/pull/1612.diff | patch -p1
     popd
 fi
 
 
 if echo $@|grep -q host
 then
+    NATIVE=true
+    ARCH=$(arch)
+    echo "building  ${SRC_DIR} for host ${ARCH}"
 
-    echo "building  ${SRCDIR} for host $(arch)"
-
-    CC=clang CXX=clang++ ${SDKROOT}/devices/x86_64/usr/bin/cmake -S ${SRCDIR} \
+    CC=clang CXX=clang++ ${SDKROOT}/devices/x86_64/usr/bin/cmake -S ${SRC_DIR} \
      -DCMAKE_BUILD_TYPE=Debug -DHAVE_AUDIO=YES -DHAVE_THREADS=NO \
-     -DHAVE_EGL=NO -DHAVE_GL=YES -DHAVE_GLX=YES -DHAVE_X11=YES -DHAVE_GLES1=NO -DHAVE_GLES2=YES \
      -DHAVE_OPENSSL=NO \
  -DHAVE_NET=NO -DWANT_NATIVE_NET=NO -DDO_PSTATS=NO \
      -DHAVE_ZLIB=YES -DHAVE_PNG=YES  \
      -DHAVE_TIFF=NO -DHAVE_JPEG=YES \
-     -DHAVE_OPUS=NO \
+ -DHAVE_AUDIO=NO \
+ -DHAVE_ASSIMP=NO -DHAVE_BULLET=NO \
+ -DHAVE_OPUS=NO -DHAVE_FFTW=NO -DHAVE_FFMPEG=NO \
      -DHAVE_HARFBUZZ=YES -DHAVE_FREETYPE=YES \
- -DHAVE_IOS_TYPEDEFS=1 -DHAVE_TINYDISPLAY=1 -DBUILD_SHARED_LIBS=YES \
+ -DHAVE_IOS_TYPEDEFS=1 -DHAVE_TINYDISPLAY=1 -DHAVE_X11=1 \
+ -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=1 \
     \
      -DCMAKE_INSTALL_PREFIX=${SDKROOT}/devices/$(arch)/usr \
-     -DHAVE_PYTHON=1 -DPython_ROOT_DIR=${SDKROOT}/devices/$(arch)/usr \
-     -DWANT_PYTHON_VERSION=${PYBUILD}
+    -DHAVE_PYTHON=0 \
+  -DHAVE_OPUS=NO -DHAVE_VORBIS=NO -DHAVE_OPENEXR=NO -DHAVE_FFMPEG=NO -DHAVE_OPENAL=NO  -DHAVE_EIGEN=NO \
+  -DHAVE_EGL=NO -DHAVE_GL=NO -DHAVE_GLX=NO -DHAVE_GLES1=NO -DHAVE_GLES2=NO
+
+
+    make && make install
+
+
+#     -DHAVE_EGL=NO -DHAVE_GL=YES -DHAVE_GLX=YES -DHAVE_X11=YES -DHAVE_GLES1=NO -DHAVE_GLES2=YES -DBUILD_SHARED_LIBS=YES \
+#     -DHAVE_PYTHON=1 -DPython_ROOT_DIR=${SDKROOT}/devices/$(arch)/usr \
+#     -DWANT_PYTHON_VERSION=${PYBUILD}
+#
 
 
 # https://cmake.org/cmake/help/v3.13/module/FindPython.html
@@ -48,26 +64,30 @@ then
 #        -DPython3_Development.Embed_FOUND=TRUE \
 #    \
 # -DHAVE_SDL2=1
+else
+    NATIVE=false
 
-
-    exit 0
 fi
 
-
+if $NATIVE
+then
+    echo
+else
 
 if echo $EMSDK|grep -q emsdk
 then
     echo building with emsdk
-
-    EMCC_CFLAGS="-sNODERAWFS" emcmake ${SDKROOT}/devices/x86_64/usr/bin/cmake ${SRCDIR} \
+    ARCH=emsdk
+    EMCC_CFLAGS="-sNODERAWFS" emcmake ${SDKROOT}/devices/x86_64/usr/bin/cmake ${SRC_DIR} \
      -DCMAKE_BUILD_TYPE=Release \
      -DHAVE_THREADS=NO \
      -DHAVE_EGL=NO -DHAVE_GL=NO -DHAVE_GLX=NO -DHAVE_X11=NO -DHAVE_GLES1=NO -DHAVE_GLES2=NO \
      -DHAVE_OPENSSL=NO \
  -DHAVE_NET=NO -DWANT_NATIVE_NET=NO -DDO_PSTATS=NO \
+ -DHAVE_AUDIO=YES \
+ -DHAVE_OPUS=NO -DHAVE_FFTW=NO -DHAVE_FFMPEG=NO \
      -DHAVE_ZLIB=1 -DHAVE_PNG=NO  \
      -DHAVE_TIFF=NO -DHAVE_JPEG=NO \
-     -DHAVE_AUDIO=NO -DHAVE_OPUS=NO \
      -DHAVE_HARFBUZZ=1 -DHAVE_FREETYPE=1 \
  -DHAVE_IOS_TYPEDEFS=1 -DHAVE_TINYDISPLAY=1 \
     \
@@ -78,12 +98,7 @@ then
 #    -DTHIRDPARTY_DIRECTORY=${PREFIX} \
 
 else
-    pushd ${SDKROOT:-/opt/python-wasm-sdk}
-
-    . scripts/wasisdk-fetch.sh
-
-    popd
-
+    . ${SDKROOT}/wasisdk/wasisdk_env.sh
 
     mkdir -p wasmbin
     cat > wasmbin/interrogate << END
@@ -120,21 +135,18 @@ END
 
 
 "
-
-    ${SDKROOT}/devices/x86_64/usr/bin/cmake ${SRCDIR} \
+    ARCH=wasisdk
+    CC=wasi-c CXX=wasi-c++ ${SDKROOT}/devices/x86_64/usr/bin/cmake ${SRC_DIR} \
      -DCMAKE_BUILD_TYPE=Release \
      -DHAVE_THREADS=NO \
      -DHAVE_EGL=NO -DHAVE_GL=NO -DHAVE_GLX=NO -DHAVE_X11=NO -DHAVE_GLES1=NO -DHAVE_GLES2=NO \
      -DHAVE_OPENSSL=NO \
  -DHAVE_AUDIO=1 -DHAVE_PYTHON=0\
- -DHAVE_OPUS=NO \
+ -DHAVE_OPUS=NO -DHAVE_FFTW=NO -DHAVE_FFMPEG=NO \
      -DHAVE_HARFBUZZ=NO -DHAVE_FREETYPE=NO \
      -DPHAVE_IOSTREAM=1 -DHAVE_TINYDISPLAY=1 -DHAVE_TIFF=NO  \
     \
     -DCMAKE_SYSTEM_NAME=WASI \
-    -DWASISDK=${SDKROOT}/wasisdk \
-    -DWASI_SDK_PREFIX=${WASI_SDK_PREFIX} \
-    \
      -DHAVE_NET=NO -DWANT_NATIVE_NET=NO -DDO_PSTATS=NO \
      -DHAVE_PYTHON=NO -DZLIB_ROOT=${WASI_SYSROOT} \
     \
@@ -142,20 +154,14 @@ END
      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 
 
+
+
 #     -DHAVE_PYTHON=1 -DPython_ROOT_DIR=${SDKROOT}/devices/wasisdk/usr \
 #     -DWANT_PYTHON_VERSION=${PYBUILD}
-
-
 #      -DHAVE_ZLIB=0 -DHAVE_PNG=NO -DHAVE_JPEG=NO \
-# -DCMAKE_SYSTEM_NAME=WASI
-#     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
-
 # not enough
 #  -DCMAKE_TOOLCHAIN_FILE=/opt/python-wasm-sdk/wasisdk/upstream/share/cmake/wasi-sdk.cmake
-
-
 #     -DTHIRDPARTY_DIRECTORY=${PREFIX} \
-
 
     #\
     # -DHAVE_PYTHON=1 \
@@ -173,15 +179,22 @@ END
 # -DHOST_PATH_PZIP=${HOST}/bin/pzip
 
 
-
 fi
+fi
+make && make install
 
-make
+
+cp -vf ${SRC_DIR}/panda/src/event/asyncTaskManager.h ${SRC_DIR}/panda/src/tinydisplay/config_tinydisplay.? ${SDKROOT}/devices/${ARCH}/usr/include/panda3d/
 
 
 if $CI
 then
-    ${SDKROOT}/wasisdk/bin/wasi-run $(pwd)/bin/pview.wasi -c -L -s $(pwd)/out.bmp
+    if $NATIVE
+    then
+        $(pwd)/bin/pview -c -L -s ${BUILD_DIR}/out.bmp
+    else
+        ${SDKROOT}/wasisdk/bin/wasi-run $(pwd)/bin/pview.wasi -c -L -s ${BUILD_DIR}/out.bmp
+    fi
     if md5sum out.bmp | grep -q "a0345fc5e8b77b9715df69e271e22c64"
     then
         exit 0
